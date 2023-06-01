07fdf6271b4d7999bf6d0fb2021db697e02e163b
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-01 05:36:38 +0530

    Initial

---
+++ FullTest_FE/src/app/core/lac-login/lac-login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lac-login',
+  templateUrl: './lac-login.component.html',
+  styleUrls: ['./lac-login.component.scss']
+})
+export class LacLoginComponent {
+
+
+checkboxconfig4345={
+type: 'checkbox',
+custom: false,
+checked:true,
+disabed:false,
+};
+
+}
