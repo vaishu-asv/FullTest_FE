bcc503eb88e911234570adbe7d70991fa1e8cd20
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-01 05:36:35 +0530

    Initial

---
+++ FullTest_FE/src/app/authentication/login-winnnow/login-winnnow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-winnnow',
+  templateUrl: './login-winnnow.component.html',
+  styleUrls: ['./login-winnnow.component.scss']
+})
+export class LoginWinnnowComponent {
+
+}
